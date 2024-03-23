a7338a8f12b1def5a224b805f214e797f2031cd8
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-03-24 02:21:37 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance: number, _customer: object) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance <= 2000) {
+    return `That will be ${(distance - 400) * 0.20} bucks.`;
+  } else if (distance <= 3000) {
+    return `I will gladly take your ${(distance - 2000) * 0.30} bucks.`;
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+describe('scuberGreetingForFeet()', function() {
+  it('gives customers a free sample if the ride is less than or equal to 400 feet', function() {
+    expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+  });
+
+  it('charges 20 dollars for a distance between 400 and 2000 feet', function() {
+    expect(scuberGreetingForFeet(1500)).to.equal('That will be 30 bucks.');
+  });
+
+  it('charges 30 dollars for a distance over 2000 feet', function() {
+    expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your 900 bucks.');
+  });
+
+  it('does not allow rides over 3000 feet', function() {
+    expect(scuberGreetingForFeet(3001)).to.equal('No can do.');
+  });
+});
+
+function ternaryCheckCity(city: string) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip: string) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+ 
+describre('ternaryCheckCity()', function() {
+  it('returns "Ok, sounds good." when the city is NYC', function() {
+    expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
+  });
+
+  it('should return "No go." if the destination city is not NYC', function() {
+    expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
+  });
+})
+
+describe('switchOnCharmFromTip()', function() {
+  it('should return "Thank you so much." if the tip is generous', function() {
+    expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+  });
+
+  it('should return "Thank you." if the tip is not as generous', function() {
+    expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+  });
+
+  it('should return "Bye." if anything else', function() {
+    expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+  });
+});
